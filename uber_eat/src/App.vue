dd18b9dcc06b4dcc416d5a77f4d9fdb5d4a51c1d
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-04-15 11:45:39 +0200

    [update] import style url font url and new component

--- uber_eat/src/App.vue
+++ uber_eat/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import RestoCard from "./components/RestoCard.vue";
+
 </script>
 
 <template>
   <header>
-   
+    <RestoCard />
   </header>
 
   <RouterView />
 </template>
+<style>
+@import url('./assets/styles/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+#app {
+  font-family: "Roboto", sans-serif;
+}
+</style>
 
