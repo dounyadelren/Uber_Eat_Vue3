84b318a784cd96912047e27ef12b9aa5bec974cd
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-04-15 11:48:38 +0200

    [update] app.vue -> reset margins and paddings to 0

--- uber_eat/src/App.vue
+++ uber_eat/src/App.vue
@@ -15,6 +15,12 @@ import RestoCard from "./components/RestoCard.vue";
 @import url('./assets/styles/reset.css');
 @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
 
+body {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+}
+
 #app {
   font-family: "Roboto", sans-serif;
 }
