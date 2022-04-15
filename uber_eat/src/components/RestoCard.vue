f8720fe4b350cf14c1981bb487b551bde2a6bba5
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-04-15 12:07:15 +0200

    [update] restocard.vue -> start inegration sur un resto

--- uber_eat/src/components/RestoCard.vue
+++ uber_eat/src/components/RestoCard.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="restaurant-card">
-      <p>test</p>
-  </div>
+    <div class="restaurant-card">
+        <div class="restaurant-image"></div>
+        <div class="restaurant-info">
+            <div class="top">
+                <p class="name">Subway</p>
+                <p class="note"><span>4.5</span></p>
+            </div>
+        </div>
+        <p class="time">20-30min</p>
+    </div>
 </template>
 
 <script>
@@ -12,11 +19,40 @@ export default {
 
 <style lang="scss">
 .restaurant-card {
-    width:32%;
+    width: 32%;
     height: 30vh;
 
     p {
         margin: 0px;
+    }
+}
+
+.restaurant-image {
+    background-image: url('https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==');
+    height: 70%;
+    background-size: cover;
+    background-position: center;
+}
+
+.restaurant-info {
+    .top {
+        margin: 10px 0px;
+        display: flex;
+        justify-content: space-between;
+        .note {
+            background-color: #eeeeee;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 30px;
+            width: 30px;
+            font-size: 0.8rem;
+            border-radius: 50%;
+        }
+    }
+    .time {
+        font-size: 0.9rem;
+
     }
 }
 </style>
