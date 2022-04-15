af11e31ab4690da7ac668c776c6df4a12395321b
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-04-15 14:22:57 +0200

    [update] homeview.vue --> implementation de la class restaurant pour pouvoir aller chercher des infos ds bdd.js

--- uber_eat/src/views/HomeView.vue
+++ uber_eat/src/views/HomeView.vue
@@ -1,5 +1,25 @@
-<script setup>
+<script>
 import RestoRow from "../components/RestoRow.vue"
+import Bdd from "../bdd.js"
+
+export default {
+  name: "HomeView",
+  components: {
+    RestoRow,
+  },
+  setup() {
+    class Restaurant {
+      constructor(name, note, image, drive_time) {
+        this.name = name
+        this.note = note
+        this.image = image
+        this.drive_time = drive_time
+      }
+    }
+    var resto = new Restaurant('toto', '3.5', 'url', '30min')
+    console.log(resto)
+  }
+}
 </script>
 
 <template>
