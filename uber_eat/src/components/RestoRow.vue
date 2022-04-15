3d4c915621bd0b20e2a607b30e3b70761985d2e9
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2022-04-15 13:41:34 +0200

    [add] restorow.vue --> component restorow pour iterer plusieurs resto en même temps

---
+++ uber_eat/src/components/RestoRow.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="restaurant-row">
+        <h2 class="title">Nos restaurants</h2>
+        <div class="wrapper-card">
+            <RestoCard v-for="(card, index) in 3" :key="index" />
+        </div>
+    </div>
+
+</template>
+
+<script>
+import RestoCard from "./RestoCard.vue"
+export default {
+    name: "RestoRow",
+    components: "RestoCard",
+    components: { RestoCard }
+}
+</script>
+
+<style lang="scss">
+.restaurant-row {
+    .wrapper-card {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+}
+</style>
